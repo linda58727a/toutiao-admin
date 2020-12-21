83b51c622485c49f74a1adf3d86b913ef3db09cf
Newcodemaster <842458727@qq.com>
2020-12-21 15:02:02 +0800

    项目初始化目录结构

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+// 加载全局样式文件
+import './styles/index.less'
 
 Vue.config.productionTip = false
 
+// 创建VUE 根实例
+// 把 router 配置到跟实例中
+// 通过 render 方法把 App根组件 渲染到#app 入口节点
 new Vue({
   router,
   render: h => h(App)
+  // el: '#app'  //等价于 $mount('#app)
 }).$mount('#app')
